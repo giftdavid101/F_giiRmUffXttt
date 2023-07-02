0f9d42fc644c53adf459c6a461e893cf3f905e92
Gift <giftdavid101@gmail.com>
2023-07-02 22:43:10 +0100

    footer wip

--- my-app/src/components/organisms/footer.tsx
+++ my-app/src/components/organisms/footer.tsx
@@ -2,10 +2,22 @@ import React from "react";
 import styled from "styled-components";
 
 const Footer = () => {
-  return <FooterStyle>footer</FooterStyle>;
+  return (
+    <FooterStyle>
+      <div className={"foot"}>
+        Sharpen your problem solving skills with Algorithm Spot
+      </div>
+    </FooterStyle>
+  );
 };
 
 const FooterStyle = styled.div`
+  width: 100%;
   grid-row: 3;
+  .foot {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 `;
 export default Footer;
