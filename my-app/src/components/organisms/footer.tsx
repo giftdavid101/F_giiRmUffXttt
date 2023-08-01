754b3e8b6febe86470e9955c480fd3bfed38bfde
Gift <giftdavid101@gmail.com>
2023-08-01 20:26:35 +0100

    algo hover style

--- my-app/src/components/organisms/footer.tsx
+++ my-app/src/components/organisms/footer.tsx
@@ -5,7 +5,11 @@ const Footer = () => {
   return (
     <FooterStyle>
       <div className={"foot"}>
-        Sharpen your problem solving skills with Algorithm Spot
+        Sharpen your problem solving skills with{" "}
+        <a href={"#"}>
+          {" "}
+          <span className={"algorithm-spot"}>Algorithm Spot</span>
+        </a>
       </div>
     </FooterStyle>
   );
@@ -14,10 +18,22 @@ const Footer = () => {
 const FooterStyle = styled.div`
   width: 100%;
   grid-row: 3;
+
   .foot {
     display: flex;
     justify-content: center;
     align-items: center;
+  }
+
+  .algorithm-spot {
+    margin: 0 10px;
+    cursor: pointer;
+    padding: 5px 2px;
+
+    :hover {
+      color: #fff;
+      background: #9066ba;
+    }
   }
 `;
 export default Footer;
